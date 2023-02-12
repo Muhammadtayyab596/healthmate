ff0ec090f6b9d4a42d8ea810fd777fbb45136977
MFarhatTayyab <tayyabazeemi786@gmail.com>
2023-02-12 10:40:22 -0800

    set layout

---
+++ src/pages/auth/Login.js
@@ -0,0 +1,131 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import logoImage from "../../assets/Images/logo-01.png"
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Paper
+            sx={{
+              py: 5,
+              px: 4,
+              bgcolor: "#fff",
+              borderRadius: 5,
+            }}
+          >
+            <Box
+              sx={{
+                justifyContent: 'center',
+                display: 'flex',
+              }
+              }>
+              <img src={logoImage} alt="logo" width={240} />
+            </Box>
+            <Box
+              sx={{
+                textAlign: "center",
+                my: 2
+              }}
+            >
+              <Typography
+                sx={{ fontSize: 30 }}
+              >
+                Sign In
+              </Typography>
+              <Typography sx={{ mt: 1 }} >Log in to your account to continue</Typography>
+            </Box>
+
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+            {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
+          </Paper>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,13 @@
 import { Navigate, useRoutes } from 'react-router-dom';
-// layouts
 import DashboardLayout from './layouts/dashboard';
-import SimpleLayout from './layouts/simple';
-//
-import BlogPage from './pages/BlogPage';
-import UserPage from './pages/UserPage';
-import LoginPage from './pages/LoginPage';
-import Page404 from './pages/Page404';
-import ProductsPage from './pages/ProductsPage';
-import DashboardAppPage from './pages/DashboardAppPage';
+import Page404 from './pages/dashboard/Page404';
+import DashboardAppPage from './pages/dashboard/DashboardAppPage';
+
+
+import Home from './pages/landing/Home';
+import About from './pages/landing/About';
+import Login from './pages/auth/Login';
+import Signup from './pages/auth/Signup';
 
 // ----------------------------------------------------------------------
 
@@ -20,26 +19,39 @@ export default function Router() {
       children: [
         { element: <Navigate to="/dashboard/app" />, index: true },
         { path: 'app', element: <DashboardAppPage /> },
-        { path: 'user', element: <UserPage /> },
-        { path: 'products', element: <ProductsPage /> },
-        { path: 'blog', element: <BlogPage /> },
       ],
+    },
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: 'about',
+      element: <About />,
     },
     {
       path: 'login',
-      element: <LoginPage />,
+      element: <Login />,
     },
     {
-      element: <SimpleLayout />,
-      children: [
-        { element: <Navigate to="/dashboard/app" />, index: true },
-        { path: '404', element: <Page404 /> },
-        { path: '*', element: <Navigate to="/404" /> },
-      ],
+      path: 'Sign-up',
+      element: <Signup />,
     },
+    // {
+    //   element: <SimpleLayout />,
+    //   children: [
+    //     { element: <Navigate to="/" />, index: true },
+    //     { path: '404', element: <Page404 /> },
+    //     { path: '*', element: <Navigate to="/404" /> },
+    //   ],
+    // },
     {
       path: '*',
-      element: <Navigate to="/404" replace />,
+      element: <Navigate to="/404" replace />
+    },
+    {
+      path: '/404',
+      element: <Page404 />
     },
   ]);
 
